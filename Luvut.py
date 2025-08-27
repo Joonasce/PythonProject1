a2bbf2569767bb5d009225aad28faf395f8f84e2
Joonas Eklund <Joonas.Eklund2@metropolia.fi>
2025-08-27 19:51:27 +0300

    Ensimmäinen tehtävä!

---
+++ Luvut.py
@@ -0,0 +1,16 @@
+#kokonaisluku, integer, INT
+eka = -9
+
+#kokonaisluku, pitkä, long
+toka = 12756413000
+
+#desimaaliluku, float
+kolmas = 7.28
+
+#kompleksiluku, complex
+neljäs = 3-2j
+
+print (eka)
+print (toka)
+print (kolmas)
+print (neljäs)
---
+++ esimerkki2.py
@@ -0,0 +1,11 @@
+ika_str = input("Kuinka vanha olet?" )
+ika = int(ika_str)
+
+print ("Ai, olet syntynyt", 2025-ika)
+
+#Muuntaminen tapa 2
+ika_str = input("Kuinka vanha olet?" )
+print ("Ai, olet syntynyt", 2025-ika)
+
+#muotoiltu tuloste
+print (f"Ai, olet syntynyt, {2025-ika}.")
